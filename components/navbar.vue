9e5c13b8cdb05c65c88c7d122832c98f6bf597c0
lacakp <lacakp.contact@gmail.com>
2021-12-07 13:22:04 +0700

    use vue js & Qualification Section

---
+++ components/navbar.vue
@@ -0,0 +1,90 @@
+<template>
+	<nav class="nav container">
+		<a href="#" class="nav_logo">Lacakp</a>
+
+		<div class="nav_menu" id="nav-menu">
+			<ul class="nav_list grid">
+				<li class="nav_item">
+					<a href="#home" class="nav_link nav_icon">
+						<i class="uil uil-estate"></i> Home
+					</a>
+				</li>
+				<li class="nav_item">
+					<a href="#about" class="nav_link nav_icon">
+						<i class="uil uil-user"></i> About
+					</a>
+				</li>
+				<li class="nav_item">
+					<a href="#skills" class="nav_link nav_icon">
+						<i class="uil uil-file-alt"></i> Skills
+					</a>
+				</li>
+				<li class="nav_item">
+					<a href="#services" class="nav_link nav_icon">
+						<i class="uil uil-briefcase-alt"></i> Services
+					</a>
+				</li>
+				<li class="nav_item">
+					<a href="#portfolio" class="nav_link nav_icon">
+						<i class="uil uil-scenery"></i> Portfolio
+					</a>
+				</li>
+				<li class="nav_item">
+					<a href="#contact" class="nav_link nav_icon">
+						<i class="uil uil-message"></i> Contact
+					</a>
+				</li>
+			</ul>
+			<i class="uil uil-times nav_close" id="nav-close"></i>
+		</div>
+		<div class="nav_btns">
+			<div class="nav_toggle" id="nav-toggle">
+				<i class="uil uil-apps"></i>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script>
+module.exports = {
+	mounted() {
+		// Menu Show Y Hidden
+		const navMenu = document.getElementById('nav-menu'),
+		navToggle = document.getElementById('nav-toggle'),
+		navClose = document.getElementById('nav-close')
+
+		// Menu Show
+		// Validate if constant exists
+		if(navToggle){
+			navToggle.addEventListener('click', () =>{
+				navMenu.classList.add('show-menu')
+			})
+		}
+
+		// Menu Hidden
+		// Validate if constant exists
+		if(navClose){
+			navClose.addEventListener('click', () =>{
+				navMenu.classList.remove('show-menu')
+			})
+		}
+
+		// Remove Menu Mobile
+		const navLink = document.querySelectorAll('.nav_link')
+
+		function linkAction(){
+			const navMenu = document.getElementById('nav-menu')
+			// When we click on each nav_link, we remove the show-menu class
+			navMenu.classList.remove('show-menu')
+		}
+		navLink.forEach(n => n.addEventListener('click', linkAction))
+	}
+};
+
+
+
+</script>
+
+<style>
+
+</style>
---
+++ components/qualification.vue
@@ -0,0 +1,119 @@
+<template>
+	<section class="qualification section">
+		<h2 class="section_title">Qualification</h2>
+		<span class="section_subtitle">My personal journey</span>
+
+		<div class="qualification_container container">
+			<div class="qualification_tabs">
+				<div class="qualification_button buttton--flex qualification_active" data-target="#education">
+					<i class="uil uil-graduation-cap qualification_icon"></i>
+					Education
+				</div>
+				<div class="qualification_button buttton--flex" data-target="#work">
+					<i class="uil uil-briefcase-alt qualification_icon"></i>
+					Work
+				</div>
+			</div>
+			<div class="qualification_sections">
+				<!-- Qualification Content 1 -->
+				<div class="qualification_content" data-content id="education">
+					<!-- qualification 1 -->
+					<div class="qualification_data">
+						<div>
+							
+							<h3 class="qualification_title">Mathayom</h3>
+							<span class="qualification_subtitle">Phosaiwittaya School</span>
+							<div class="qualification_calendar">
+								<i class="uil uil-calendar-alt"></i> 2013 - 2019
+							</div>
+						</div>
+						<div>
+								<span class="qualification_rounder"></span>
+								<span class="qualification_line"></span>
+							</div>
+					</div>
+					<!-- qualification 2 -->
+					<div class="qualification_data">
+						<div></div>
+						<div>
+							<span class="qualification_rounder"></span>
+							<!-- <span class="qualification_line"></span> -->
+						</div>
+						<div>
+							<h3 class="qualification_title">Computer Science</h3>
+							<span class="qualification_subtitle">Rajabhat Sakon Nakhon University</span>
+							<div class="qualification_calendar">
+								<i class="uil uil-calendar-alt"></i> 2022 - 2024
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<!-- Qualification Content 2 -->
+				<div class="qualification_content qualification_active" data-content id="work">
+										<!-- qualification 1 -->
+					<div class="qualification_data">
+						<div>
+							
+							<h3 class="qualification_title">Freelance Websraping</h3>
+							<span class="qualification_subtitle">Fastwork</span>
+							<div class="qualification_calendar">
+								<i class="uil uil-calendar-alt"></i> 2022 - Present
+							</div>
+						</div>
+						<div>
+								<span class="qualification_rounder"></span>
+								<span class="qualification_line"></span>
+							</div>
+					</div>
+					<!-- qualification 2 -->
+					<div class="qualification_data">
+						<div></div>
+						<div>
+							<span class="qualification_rounder"></span>
+							<!-- <span class="qualification_line"></span> -->
+						</div>
+						<div>
+							<h3 class="qualification_title">Developer</h3>
+							<span class="qualification_subtitle">ALPHA COMPANY</span>
+							<div class="qualification_calendar">
+								<i class="uil uil-calendar-alt"></i> 2024 - 2025
+							</div>
+						</div>
+					</div>
+				
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+module.exports = {
+	mounted(){
+		const tabs = document.querySelectorAll('[data-target]'),
+			tabsContents = document.querySelectorAll('[data-content]')
+
+		tabs.forEach(tab =>{
+			tab.addEventListener('click', () => {
+				const target = document.querySelector(tab.dataset.target)
+
+				tabsContents.forEach(tabsContent =>{
+					tabsContent.classList.remove('qualification_active')
+				})
+
+				target.classList.add('qualification_active')
+
+				tabs.forEach(tab =>{
+					tab.classList.remove('qualification_active')
+				})
+				tab.classList.add('qualification_active')
+			})
+		})
+	}
+};
+</script>
+
+<style>
+
+</style>
